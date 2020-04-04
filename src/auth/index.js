dee71b128b3b5b8b88c4dbf418133bf026b2b858
aitorgallardo <gmaitor@outlook.com>
2020-04-04 19:55:01 +0200

    login user with token

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Joi = require('joi');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -29,7 +30,7 @@ router.post('/signup', (req, res, next) => {
         const error = new Error('User already exist');
         next(error);
       } else {
-        bcrypt.hash(req.body.password, 12).then((hashedPassword) => {          
+        bcrypt.hash(req.body.password, 12).then((hashedPassword) => {
           const userBody = {
             username: req.body.username,
             password: hashedPassword,
@@ -44,6 +45,49 @@ router.post('/signup', (req, res, next) => {
     });
   } else {
     next(result.error);
+  }
+});
+
+function respondError422(res, next) {
+  res.status(422);
+  const error = new Error('Unable to login');
+  next(error);
+}
+
+router.post('/login', (req, res, next) => {
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    User.findOne({
+      username: req.body.username,
+    }).then((user) => {
+      // if user is undefined, username is not in the db, otherwise, duplicate user
+      if (user) {
+        const hashedPassword = user.password;
+        bcrypt.compare(req.body.password, hashedPassword).then((hashres) => {
+          if (hashres === true) {
+            const payload = {
+              id: user._id,
+              username: user.username,
+            };
+            console.log( process.env.TOKEN_SECRET);
+            
+            jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '1d' }, (err, token) => {
+              if (err) {
+                respondError422(res, next);
+              } else {
+                res.json({token})
+              }
+            });
+          } else {
+            respondError422(res, next);
+          }
+        });
+      } else {
+        respondError422(res, next);
+      }
+    });
+  } else {
+    respondError422(res, next);
   }
 });
 
